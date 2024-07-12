b0e09bf1a35764bc47d114fc585d7e86416c0320
Jack Hunter <jhunter5@binghamton.edu>
2024-07-12 16:53:32 -0400

    need to update jsx

---
+++ digport/src/pages/HomePage.jsx
@@ -0,0 +1,9 @@
+import Navbar from '../components/Navbar';
+
+const HomePage = () => {
+    return(
+        <div className="min-h-screen flex flex-col">
+            <Navbar />
+        </div>
+    );
+};
