66c513faf70d25c916f43d5e013292470f706803
Jack Hunter <jhunter5@binghamton.edu>
2024-07-13 19:22:03 -0400

    updated about

--- dev/personal/src/components/About.jsx
+++ dev/personal/src/components/About.jsx
@@ -4,7 +4,7 @@ const About = () => {
             <h1 className="text-sky-500 font-bold">Background:</h1>
             <p>I am a rising junior in my degree program. I have interests in many subfields of Computer Science. In particular, I love learning about software development, computer and information security, data science, and machine learning. Additionally, I enjoy studying math, where I find interests in abstract algebra and statistics/probability. I strongly believe that the study of higher-level math leads to better problem solving skills that are applicable to any situation.</p>
             <h1 className="text-sky-500 font-bold">Hobbies:</h1>
-            <p>In my free time, I love to stay active. I love training for distance running, rock climbing, and playing pickup basketball. Being local to the Adirondack Mountains and having family in Colorado, I love to get outside and hike when I can as well.</p>
+            <p>In my free time, I love to stay active. I love training for distance running, rock climbing, and playing pickup basketball. Being local to the Adirondack Mountains and having family in Colorado, I love to get outside and hike when I can as well. I'm always listening to new music and reading too, my Spotify and Goodreads Accounts can be found under 'Connect'</p>
         </div>
     )
 };
